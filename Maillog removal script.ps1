003bbeb5f73d5c02158b32061829e2580030334b
ageier997 <77015581+ageier997@users.noreply.github.com>
2021-01-05 11:25:36 -0500

    Add files via upload

---
+++ Maillog removal script.ps1
@@ -0,0 +1,8 @@
+﻿
+
+#change logpath
+#change *.txt to log type
+
+$logpath = "C:\Users\alexg\Desktop\AlexTest"
+Get-ChildItem $logpath -recurse *.txt -force | where {$_.LastWriteTime -gt (Get-Date).AddDays(30)}
+Remove-Item -force
